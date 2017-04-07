25bafabf7e91b228ca3cefa1d55986e2b64bb6ba
RamiBenMohamed <benmohamedrami91@gmail.com>
2017-04-07 15:15:56 +0200

    Delete StudentController.java

--- AWS/src/main/java/com/Rami/StudentController.java
+++
@@ -1,8 +0,0 @@
-package com.Rami;
-public class StudentController {
-	private StudentService studentService;
-	public Collection <Student>getAllStudents(){
-		return studentService.getAllStudents();
-	}
-
-}
